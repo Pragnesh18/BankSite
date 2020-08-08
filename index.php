76287796931a1a67980cb6faa4aed243b3db8ca6
Pragnesh18 <37249145+Pragnesh18@users.noreply.github.com>
2020-08-09 01:47:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/admin_home.php/');
+	header('Location: '.$uri.'/home.php');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
